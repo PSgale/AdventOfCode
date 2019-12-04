588d96ee61722c280f3c69b3bcc45eddc6d297d6
Pavel Samusenko <pavel.samusenko@gmail.com>
2019-12-04 22:04:00 +0100

    password brute-force

---
+++ 2019/04/guess_password.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+# Function to check password criteria:
+# 1. Two adjacent digits are the same
+# 2. Digits are never decrease
+def check_main_criteria(number):
+    numbers_ = np.asarray([int(char) for char in str(number)])
+    _numbers = numbers_[1:]
+    numbers_ = numbers_[:-1]
+
+    if all([x >= 0 for x in _numbers - numbers_]) and \
+            any([x == 0 for x in _numbers - numbers_]):
+        return True
+    return False
+
+
+# Function to check password criteria:
+# 1. The two adjacent matching digits are not part of a larger group of matching digits
+def check_last_criteria(number):
+    numbers_ = np.asarray([int(char) for char in str(number)])
+    _numbers = numbers_[1:]
+    numbers_ = numbers_[:-1]
+
+    snumber = ''.join(['1' if x == 0 else '0' for x in _numbers - numbers_])
+    if snumber[:2] == '10' or snumber[-2:] == '01' or \
+            '010' in snumber:
+        return True
+    return False
+
+
+print("%%% RUN %%%")
+first_number = 134792
+last_number = 675810
+number_of_passwords = 0
+number_of_passwords_2 = 0
+
+current = first_number
+while current <= last_number:
+    if check_main_criteria(current):
+        number_of_passwords += 1
+        # print(current)
+        if check_last_criteria(current):
+            number_of_passwords_2 += 1
+            print(current)
+
+    current += 1
+
+print("Number of passwords: " + str(number_of_passwords))
+print("Number of passwords with last criteria: " + str(number_of_passwords_2))
