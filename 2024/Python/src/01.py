bd0046b0ca5e89627e875aeb7018e1d4b764a829
Pavel <pavel.samusenko@gmail.com>
2024-12-02 23:08:21 +0100

    2024 day1 (refactoring)

--- 2024/Python/src/01.py
+++ 2024/Python/src/01.py
@@ -1,43 +1,40 @@
 from helpers import readFileLines
 from collections import Counter
-import re
 
-def getDistance(file, isDebug = False):
+def getDistanceAndSimilarity(file, isDebug = False):
     lines = readFileLines(file)
 
-    a = []
-    b = []
+    a_list = []
+    b_list = []
     # Load data
     for str in lines:
         numbers = str.split('  ')
-        a.append(int(numbers[0]))
-        b.append(int(numbers[1]))
+        a_list.append(int(numbers[0]))
+        b_list.append(int(numbers[1]))
 
     # Sort elements in lists for distance calculation
-    a.sort()
-    b.sort()
+    a_list.sort()
+    b_list.sort()
 
-    # Calculate frequency of elements in list b
-    key_b = Counter(b)
+    # Calculate frequency of elements in list b_list
+    b_list_to_frequencies = Counter(b_list)
 
     total_distance = 0
     total_similarity = 0
-    # Calculate distance
-    for i, val_a in enumerate(a):
-        val_b = b[i]
-
-        total_distance += abs(val_a - val_b)
-        total_similarity += val_a * key_b[val_a]
+    # Calculate distance and similarity
+    for a, b in zip(a_list, b_list):
+        total_distance += abs(a - b)
+        total_similarity += a * b_list_to_frequencies[a]
 
     if (isDebug):
-        print(a)
-        print(b)
+        print(a_list)
+        print(b_list)
 
     return [total_distance, total_similarity]
 
 
 print("%%% Test %%%")
-value1, value2 = getDistance("01-two-list-locations-t1.txt", True)
+value1, value2 = getDistanceAndSimilarity("01-two-list-locations-t1.txt", True)
 
 Expected = 11
 print("Distance: ", value1)
@@ -46,6 +43,6 @@ assert value1 == Expected, "Not expected result."
 
 
 print("%%% Unit %%%")
-value1, value2 = getDistance("01-two-list-locations-p1.txt")
+value1, value2 = getDistanceAndSimilarity("01-two-list-locations-p1.txt")
 print("Distance: ", value1)
 print("Similarity: ", value2)
